0365629151ef2f276e198b2c71a9297ac707a2ec
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-03 17:33:32 +0100

    finished first attempt

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
+     maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
+     allNames = femaleNames.concat(maleNames),
+     newName = 'Marian';
+console.log(allNames);
+if (allNames.indexOf(newName) === -1) {
+    newName = allNames.push(newName)
+};
